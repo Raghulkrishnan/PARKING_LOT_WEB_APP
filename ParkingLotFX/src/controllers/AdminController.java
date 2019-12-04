1fd673ead2cb1bee293a0c837a0cb0461d630d55
shrut <shrut@104.194.115.199>
2019-12-03 19:42:24 -0600

    .

--- ParkingLotFX/src/controllers/AdminController.java
+++ ParkingLotFX/src/controllers/AdminController.java
@@ -4,8 +4,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Connection;
-
 import Dao.DBConnect;
 import application.DynamicTable;
 import application.Main;
